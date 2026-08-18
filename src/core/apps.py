d2ef6425134b27d3663d92dd7ed213c868d9f225
Renzo León <renzoxdgamer@gmail.com>
2026-08-18 16:08:17 -0500

    Agrega pruebas unitarias y configuracion de la app core

--- src/core/apps.py
+++ src/core/apps.py
@@ -2,5 +2,8 @@ from django.apps import AppConfig
 
 
 class CoreConfig(AppConfig):
+    """Configuración de la aplicación principal del Sistema Empresarial."""
+
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+    verbose_name = 'Catálogo'
